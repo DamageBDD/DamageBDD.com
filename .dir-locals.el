3b5bf4c25930bf965939deda149f6acc2e33c26a
Steven Joseph (asyncmind) <steven@damagebdd.com>
2025-08-02 18:29:37 +1000

    fix: dirlocals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ;; ~/Org/damagebdd/.dir-locals.el
 ((org-mode
-  . ((eval . (load-file "./scripts/publish.el")))))
+  . ((eval . (load-file (expand-file-name "scripts/publish.el"
+                                          (locate-dominating-file buffer-file-name ".dir-locals.el")))))))
